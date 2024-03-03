4407e81e93d6d52dc07a08e7642be4a73526efdd
Jijix <eva.trapani@yahoo.fr>
2024-03-03 19:42:58 +0100

    Correction normalement du caca de Eva (Le labyrinthe se génère)

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ class Case:
 
 
 # Première fonction pure !
-def generate_list (x_max, y_max):
+def generate_list (nb_lines, nb_columns):
     labyrinthe = []
-    for i in range (x_max):
-        for j in range (y_max):
-            case = Case(i, j)
+    for y in range (nb_lines):
+        for x in range (nb_columns):
+            case = Case(x, y)
             labyrinthe.append(case)
     
     return labyrinthe
 
-labyrinthe = generate_list(5, 5)
 
 def go_through_list (labyrinthe):
     for case in labyrinthe:
@@ -24,37 +23,46 @@ def go_through_list (labyrinthe):
 
 
 # Fonction Closure !
-def generate_path (case):
+def generate_path (case, x_max, y_max):
     good_path = []
     good_path.append(case)
 
-    def neighbor (case):
+    def neighbor(case, x_max, y_max):
         neighbors = []
         # on ajoute les voisins (haut, bas, droite, gauche)
-        neighbors.append(Case(case.x, case.y + 1))
-        neighbors.append(Case(case.x, case.y - 1))
-        neighbors.append(Case(case.x + 1, case.y))
-        neighbors.append(Case(case.x - 1, case.y))
+        if case.x + 1 < x_max:
+            neighbors.append(Case(case.x + 1, case.y))  # bas
+        if case.x - 1 >= 0:
+            neighbors.append(Case(case.x - 1, case.y))  # haut
+        if case.y + 1 < y_max:
+            neighbors.append(Case(case.x, case.y + 1))  # droite
+        if case.y - 1 >= 0:
+            neighbors.append(Case(case.x, case.y - 1))  # gauche
         return neighbors
-    
 
     # on met la case de départ dans le chemin
     path = [case]
+    print ("first path : ", path[0].x, path[0].y)
 
     # tant que j'ai des voisin à visiter
     while path: 
         # je récupère la case où je suis
         actual_case = path[-1]
+        print ("actual case : ", actual_case.x, actual_case.y)
         # je récupère ses voisins
-        neighbors = neighbor(actual_case)
+        neighbors = neighbor(actual_case, x_max, y_max)
+        for i in neighbors:
+            print("neighbor : ", i.x, i.y)
         # je crée une liste pour les voisins que j'ai pas visité
         unvisited_neighbors = []  
+        print("good_path avant l'itération sur les voisins :", [(case.x, case.y) for case in good_path])
         # pour chaque voisin de ma case actuelle
-        for n in neighbors:
+        for i in neighbors:
             # si je n'ai pas encore visité ce voisin
-            if n not in good_path:
+            if (i.x, i.y) not in [(case.x, case.y) for case in good_path]:
                 # je l'ajoute à ma liste de voisin non visité
-                unvisited_neighbors.append(n)
+                unvisited_neighbors.append(i)
+                print ("unvisited neighbor : ", i.x, i.y)
 
         # si j'ai des voisins non visité
         if unvisited_neighbors:
@@ -64,6 +72,8 @@ def generate_path (case):
             path.append(next_case)
             # et je l'ajoute aussi à la liste du bon chemin
             good_path.append(next_case)
+            for i in good_path:
+                print("good path : ", i.x, i.y)
         else:
             # sinon je retourne en arrière et je supprime la case actuelle de mon chemin
             path.pop()
@@ -73,12 +83,15 @@ def generate_path (case):
 
 # on génère la grille
 labyrinthe = generate_list(5, 5)
+# on parcourt la grille
+go_through_list(labyrinthe)
 # on choisi une case de départ
 first_case = Case(0, 0)
 # on génère le chemin
-path = generate_path(first_case)
+path = generate_path(first_case, 5, 5)
 
 # on affiche le chemin
+print ("Chemin : ")
 for case in path:
     print(case.x, case.y)
     
