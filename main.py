6cda912b62fe6de7879cc69a7a98c9303fb6e6b6
Jijix <eva.trapani@yahoo.fr>
2024-03-03 20:34:06 +0100

    début de la création d'affichage du labyrinthe (PETIT DEBUT HEIN)

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def generate_path (case, x_max, y_max):
             print("neighbor : ", i.x, i.y)
         # je crée une liste pour les voisins que j'ai pas visité
         unvisited_neighbors = []  
+        
         print("good_path avant l'itération sur les voisins :", [(case.x, case.y) for case in good_path])
         # pour chaque voisin de ma case actuelle
         for i in neighbors:
@@ -68,6 +69,8 @@ def generate_path (case, x_max, y_max):
         if unvisited_neighbors:
             # je choisi un voisin au hasard
             next_case = random.choice(unvisited_neighbors)
+            print ("choix parmi :", [(case.x, case.y) for case in unvisited_neighbors])
+            print ("next case : ", next_case.x, next_case.y)
             # je l'ajoute à mon chemin (path)
             path.append(next_case)
             # et je l'ajoute aussi à la liste du bon chemin
@@ -80,9 +83,39 @@ def generate_path (case, x_max, y_max):
 
     return good_path
 
+    
+def print_grid(labyrinthe):
+    # on recup le nombre de lignes et de colonnes
+    nb_lines = max(case.y for case in labyrinthe) + 1
+    nb_columns = max(case.x for case in labyrinthe) + 1
+
+    # Pour chaque ligne
+    for y in range(nb_lines):
+        # on crée une ligne vide
+        line = ""
+
+        # Pour chaque colonne
+        for x in range(nb_columns):
+            # on regarde si la case existe
+            if any(case.x == x and case.y == y for case in labyrinthe):
+                # Si la case existe, on fait un espace
+                line += " "
+            else:
+                # Sinon on met un mur
+                line += "|"
+
+            # on met un mur à droite de la case
+            line += "| "
+
+        # Afficher la ligne dans la console
+        print(line)
+
+
+
 
 # on génère la grille
 labyrinthe = generate_list(5, 5)
+print ("Labyrinthe : ", [(case.x, case.y) for case in labyrinthe])
 # on parcourt la grille
 go_through_list(labyrinthe)
 # on choisi une case de départ
@@ -91,7 +124,9 @@ first_case = Case(0, 0)
 path = generate_path(first_case, 5, 5)
 
 # on affiche le chemin
-print ("Chemin : ")
-for case in path:
-    print(case.x, case.y)
+print ("Chemin : ", [(case.x, case.y) for case in path])
+
+# on affiche le labyrinthe
+print("\nLabyrinthe : ")
+print_grid(labyrinthe)
     
